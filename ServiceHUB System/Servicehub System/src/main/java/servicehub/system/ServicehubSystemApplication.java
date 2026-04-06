11449e8008444e8a446f204894ad30fa7f4e5ef1
shihab-sani <mdsani633@gmail.com>
2026-04-06 12:15:44 +0600

    Initialize Spring Boot project for Servicehub System.
    Includes application YAML, Maven configuration, .gitignore, IntelliJ project files, and basic structure with main and test classes.

---
+++ ServiceHUB System/Servicehub System/src/main/java/servicehub/system/ServicehubSystemApplication.java
@@ -0,0 +1,13 @@
+package servicehub.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicehubSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServicehubSystemApplication.class, args);
+    }
+
+}
---
+++ ServiceHUB System/Servicehub System/src/test/java/servicehub/system/ServicehubSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package servicehub.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicehubSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
