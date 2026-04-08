78f9afd2bc66cc64bbe649068c1b1a5d9f5e810b
shihab-sani <mdsani633@gmail.com>
2026-04-09 05:24:18 +0600

    Add RestController, RequestMapping, CrossOrigin, and AllArgsConstructor annotations to AuthController, BookingController, and WorkerController

--- ServiceHUB System/Servicehub System/src/main/java/servicehub/system/Controller/AuthController.java
+++ ServiceHUB System/Servicehub System/src/main/java/servicehub/system/Controller/AuthController.java
@@ -1,4 +1,14 @@
 package servicehub.system.Controller;
 
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@CrossOrigin(origins = "*")
+@AllArgsConstructor
 public class AuthController {
+
 }
--- ServiceHUB System/Servicehub System/src/main/java/servicehub/system/Controller/WorkerController.java
+++ ServiceHUB System/Servicehub System/src/main/java/servicehub/system/Controller/WorkerController.java
@@ -1,4 +1,13 @@
 package servicehub.system.Controller;
 
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/worker")
+@CrossOrigin(origins = "*")
+@AllArgsConstructor
 public class WorkerController {
 }
