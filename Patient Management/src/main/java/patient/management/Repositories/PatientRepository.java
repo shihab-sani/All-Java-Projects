f0919712a92947dd89a6dba0d7a9b31334754ee2
shihab-sani <mdsani633@gmail.com>
2026-05-27 00:49:15 +0600

    Add PatientRepository interface extending JpaRepository

---
+++ Patient Management/src/main/java/patient/management/Repositories/PatientRepository.java
@@ -0,0 +1,11 @@
+package patient.management.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import patient.management.ModelClasses.Patient;
+
+import java.util.UUID;
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, UUID> {
+}
