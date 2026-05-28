696339f274d4e7e479f4222361e3591ef701b6f4
shihab-sani <mdsani633@gmail.com>
2026-05-28 16:32:59 +0600

    Handle EmailAlreadyExistsException in GlobalExceptionHandler, add corresponding exception class, and enable logging with Lombok @Slf4j

--- Patient Management/src/main/java/patient/management/ExceptionHandler/GlobalExceptionHandler.java
+++ Patient Management/src/main/java/patient/management/ExceptionHandler/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package patient.management.ExceptionHandler;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -19,5 +21,14 @@ public class GlobalExceptionHandler {
                 errors.put(error.getField(), error.getDefaultMessage()));
 
         return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(EmailAlreadyExitsException.class)
+    public ResponseEntity<Map<String, String>> handleEmailAlreadyExistsException(EmailAlreadyExitsException ex){
+
+        log.warn("Email already exists: {}", ex.getMessage());
+        Map<String, String> error = new HashMap<>();
+        error.put("error", "Email already exists.");
+        return ResponseEntity.badRequest().body(error);
     }
 }
